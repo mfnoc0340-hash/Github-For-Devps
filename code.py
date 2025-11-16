d5345748c1a7b732637f223f764a67ee67f822ee
Hammad <root@ip-172-31-2-54.ap-south-1.compute.internal>
2025-11-16 06:03:34 +0000

    This Third Commit

---
+++ code.py
@@ -0,0 +1,23 @@
+# Basic Python Program
+
+# Print statement
+print("Welcome to Python!")
+
+# Variables
+name = "Ali"
+age = 22
+
+print("Name:", name)
+print("Age:", age)
+
+# Taking user input
+user_name = input("Enter your name: ")
+print("Hello,", user_name)
+
+# Simple calculation
+num1 = 10
+num2 = 5
+result = num1 + num2
+
+print("Sum =", result)
+
